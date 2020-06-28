f438f17ae0e2f295c348b7eba4615a7863be4e7d
saurabh <saurabhbharti9@gmail.com>
2020-06-29 02:14:18 +0530

    added check for email uniqueness

--- controller/index.js
+++ controller/index.js
@@ -4,15 +4,22 @@ const { formatResponse } = require("../lib");
 const { registrationValidation } = require("../validation");
 exports.register = async (req, res) => {
   const { name, email, password } = req.body;
+  //create new user
   let newuser = new User({
     name: name,
     email: email,
     password: password,
   });
+  //validate data
   let { error } = registrationValidation(req.body);
   if (error)
     return res.json(formatResponse(true, error.details[0].message, null));
-
+  //check for email uniqueness
+  let emailExists = await User.find({ email: email });
+  if (emailExists)
+    return res
+      .status(400)
+      .json(formatResponse(true, "Email Already Exists", email));
   //save to db
   try {
     let savedUser = await User.create(newuser);

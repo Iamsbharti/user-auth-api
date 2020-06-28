7444732e86f0c681935bb1c7f6e28dcdbffb7514
saurabh <saurabhbharti9@gmail.com>
2020-06-28 15:52:34 +0530

    init db

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-
+const { initdb } = require("./initdb");
+//configure env variables
 dotenv.config();
+//init express
 const app = express();
 
+//init db
+initdb();
+//server listens
 app.listen(process.env.SERVER_PORT, () =>
   console.log("Server up and running at", process.env.SERVER_PORT)
 );

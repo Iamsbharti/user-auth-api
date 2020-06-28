139501055efb953dd9ebb2d80858238c809e1a59
saurabh <saurabhbharti9@gmail.com>
2020-06-29 02:24:11 +0530

    implementes hashing password

--- controller/index.js
+++ controller/index.js
@@ -2,24 +2,36 @@ const mongoose = require("mongoose");
 const User = require("../models/User");
 const { formatResponse } = require("../lib");
 const { registrationValidation } = require("../validation");
+const bcrypt = require("bcryptjs");
+
 exports.register = async (req, res) => {
   const { name, email, password } = req.body;
-  //create new user
-  let newuser = new User({
-    name: name,
-    email: email,
-    password: password,
-  });
+  console.log("control", name, email, password);
   //validate data
   let { error } = registrationValidation(req.body);
+  console.log("error", error);
   if (error)
     return res.json(formatResponse(true, error.details[0].message, null));
+
   //check for email uniqueness
-  let emailExists = await User.find({ email: email });
+  let emailExists = await User.findOne({ email: email });
+  console.log("email", emailExists);
   if (emailExists)
     return res
       .status(400)
       .json(formatResponse(true, "Email Already Exists", email));
+
+  //hash password
+  let salt = await bcrypt.genSalt(10);
+  let hashedPassword = await bcrypt.hash(password, salt);
+
+  //create new user
+  let newuser = new User({
+    name: name,
+    email: email,
+    password: hashedPassword,
+  });
+
   //save to db
   try {
     let savedUser = await User.create(newuser);
